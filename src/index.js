cde761773b0f23d320eda3dfaf9a667f7b1c0a0a
Noah Reece <noah.w.reece@gmail.com>
2022-03-13 15:47:54 -0400

    create animation functions

--- src/index.js
+++ src/index.js
@@ -9,7 +9,25 @@ const yawn1 = "./images/tuba-yawn-1.png";
 const yawn2 = "./images/tuba-yawn-2.png";
 const yawn3 = "./images/tuba-yawn-3.png";
 
+// reference to frame sequences and animations
+const curlAnim = [idle, curl1, curl2, curl1, idle];
+
+// animation variables
+const updateInterval = 0.15;
+
 // changes DOM image to specified image path
-const updateSprite = imgPath => tuba.setAttribute('src', imgPath);
+const updateImage = (imgElement, newImgPath) => imgElement.setAttribute('src', newImgPath);
 
+// play the given animation
+function runAnimation(image, animation) {
+    let frame = 0;
+    stepAnimation();
+    const anim = setInterval(stepAnimation, updateInterval * 1000);
 
+    function stepAnimation() {
+        updateImage(image, animation[frame]);
+        ++frame >= animation.length ? clearInterval(anim): null;
+    }
+}
+
+runAnimation(tuba, curlAnim);
