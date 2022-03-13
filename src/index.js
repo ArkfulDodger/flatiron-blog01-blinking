b262b9d273bad5508eb89286621d6e1f4c9592f0
Noah Reece <noah.w.reece@gmail.com>
2022-03-13 14:25:16 -0400

    get refs to images and create changeSprite f(x)

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// reference to image in DOM
+const tuba = document.getElementById('tuba');
+
+// reference to individual sprite paths
+const idle = "./images/tuba-idle.png";
+const curl1 = "./images/tuba-curl-1.png";
+const curl2 = "./images/tuba-curl-2.png";
+const yawn1 = "./images/tuba-yawn-1.png";
+const yawn2 = "./images/tuba-yawn-2.png";
+const yawn3 = "./images/tuba-yawn-3.png";
+
+// changes DOM image to specified image path
+function changeSpriteTo(imgPath) {
+    tuba.setAttribute('src', imgPath);
+}
